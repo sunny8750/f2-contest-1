0fa391dee3c0c5d23b215c743d776aaeae9b30d1
sunny8750 <sunnyrao8750@gmail.com>
2023-01-07 17:11:50 +0530

    solution

--- script.js
+++ script.js
@@ -5,23 +5,47 @@ let arr = [
   { id: 2, name: "jack", age: "20", profession: "developer" },
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
+let arr1 = [
+  { id: 1, name: "karan", age: "19", profession: "manager" },
+  { id: 2, name: "sumit", age: "28", profession: "sr.manager" },
+  { id: 3, name: "vivek", age: "25", profession: "IT Manager" },
+];
+let arr2 = [
+  { id: 1, name: "anand", age: "27", profession: "FSO" },
+  { id: 2, name: "gayatri", age: "23", profession: "Trainer" },
+  { id: 3, name: "sandeep", age: "21", profession: "VP" },
+];
 
-function PrintDeveloperswithMap() {
-  //Write your code here
+
+
+function PrintDeveloperswithMap(item) {
+ arr.map(function(arr){
+  if(arr.profession=='developer'){
+    console.log(arr)
+  }
+ })
+
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+  arr.forEach((arr)=>{
+    if(arr.profession=='developer'){
+      console.log(arr)
+    }
+  })
 }
 
 function addData() {
-  //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"})
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here
+  arr.pop()
+  console.log(arr);
 }
 
 function concatenateArray() {
-  //Write your code here
+  let x =arr1.concat(arr2);
+  console.log(x);
 }
